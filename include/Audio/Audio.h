8a230574d64c6cdf2e9519f407ab1d6b2cef3340
Steak <steakisawake@gitlab.com>
2020-02-18 15:45:09 -0500

    more cmake fixes

---
+++ include/Audio/Audio.h
@@ -0,0 +1,10 @@
+#ifndef AUDIO_THEBASE_H_
+#define AUDIO_THEBASE_H_
+
+#include <portaudio.h>
+
+bool Audio_Init();
+
+void Audio_Shutdown();
+
+#endif
---
+++ src/Audio/Audio.c
@@ -0,0 +1,6 @@
+#include "Audio.h"
+
+bool Audio_Init()
+{
+	
+}
