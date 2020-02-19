b3e2bea003ff1609f34bcd803f391407af9f253c
Steak <STEAKISAWAKE@users.noreply.github.com>
2020-02-18 22:11:53 -0500

    audio works!

--- include/Audio/Audio.h
+++ include/Audio/Audio.h
@@ -1,10 +1,34 @@
 #ifndef AUDIO_THEBASE_H_
 #define AUDIO_THEBASE_H_
 
+#include <stdbool.h>
+
+#include <sndfile.h>
 #include <portaudio.h>
+
+#define FRAMES_PER_BUFFER   (512)
+
+typedef struct
+{
+    SNDFILE*    file;
+    SF_INFO     info;
+    PaStream*   stream;
+} Sound;
 
 bool Audio_Init();
 
+void Audio_LoadFile(const char*, Sound*);
+
+void Audio_Play(Sound*);
+
+void Audio_Stop(Sound*);
+
 void Audio_Shutdown();
+
+static int callback(const void* input, void* output,
+    unsigned long frameCount,
+    const PaStreamCallbackTimeInfo* timeInfo,
+    PaStreamCallbackFlags statusFlags,
+    void* userData);
 
 #endif
--- src/Audio/Audio.c
+++ src/Audio/Audio.c
@@ -1,6 +1,130 @@
 #include "Audio.h"
 
+#include <stdio.h>
+
 bool Audio_Init()
 {
-	
+
+    PaError error;
+
+    /* init portaudio */
+    error = Pa_Initialize();
+    if (error != paNoError)
+    {
+        fprintf(stderr, "Problem initializing Audio Module.\n");
+        return false;
+    }
+
+    return true;
+}
+
+void Audio_LoadFile(const char* file, Sound* data)
+{
+
+
+    /* Open the soundfile */
+    data->file = sf_open(file, SFM_READ, &data->info);
+    if (sf_error(data->file) != SF_ERR_NO_ERROR)
+    {
+        fprintf(stderr, "%s\n", sf_strerror(data->file));
+        fprintf(stderr, "File: %s\n", file);
+        return; // Error could not load file
+    }
+
+    
+    return;
+
+}
+
+void Audio_Play(Sound* data)
+{
+    PaError error;
+
+    /* Open PaStream with values read from the file */
+    error = Pa_OpenDefaultStream(&data->stream
+        , 0                     /* no input */
+        , data->info.channels         /* stereo out */
+        , paFloat32             /* floating point */
+        , data->info.samplerate
+        , FRAMES_PER_BUFFER
+        , callback
+        , data);        /* our sndfile data struct */
+    if (error != paNoError)
+    {
+        fprintf(stderr, "Problem opening Default Stream\n");
+        return;
+    }
+
+    /* Start the stream */
+    error = Pa_StartStream(data->stream);
+    if (error != paNoError)
+    {
+        fprintf(stderr, "Problem opening starting Stream\n");
+        return;
+    }
+
+    return;
+}
+
+void Audio_Stop(Sound* data)
+{
+    PaError error;
+
+    /* Close the soundfile */
+    sf_close(data->file);
+
+    /*  Shut down portaudio */
+    error = Pa_CloseStream(data->stream);
+    if (error != paNoError)
+    {
+        fprintf(stderr, "Problem closing stream\n");
+        return;
+    }
+}
+
+void Audio_Shutdown()
+{
+    PaError error;
+
+    error = Pa_Terminate();
+    if (error != paNoError)
+    {
+        fprintf(stderr, "Problem terminating\n");
+        return;
+    }
+}
+
+
+static
+int
+callback
+(const void* input
+    , void* output
+    , unsigned long                   frameCount
+    , const PaStreamCallbackTimeInfo* timeInfo
+    , PaStreamCallbackFlags           statusFlags
+    , void* userData
+)
+{
+    float* out;
+    Sound* p_data = (Sound*)userData;
+    sf_count_t       num_read;
+
+    out = (float*)output;
+    p_data = (Sound*)userData;
+
+    /* clear output buffer */
+    memset(out, 0, sizeof(float) * frameCount * p_data->info.channels);
+
+    /* read directly into output buffer */
+    num_read = sf_read_float(p_data->file, out, (int)frameCount * (int)p_data->info.channels);
+
+
+    /*  If we couldn't read a full frameCount of samples we've reached EOF */
+    if (num_read < frameCount)
+    {
+        return paComplete;
+    }
+
+    return paContinue;
 }
